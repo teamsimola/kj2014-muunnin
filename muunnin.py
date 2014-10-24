ea6e93f195a1959f1afbd28d9fd9fb2c991c7900
Aleksi Kervinen <aleksi.kervinen@lut.fi>
2014-10-24 14:05:59 +0300

    more structuring

--- muunnin.py
+++ muunnin.py
@@ -13,13 +13,25 @@ def f_to_c(f):
 	return (f - 32) * 5.0/9.0
 	
 units = [
-	"Paino",
-	"Lampotila"
+	{
+		'name': "Paino",
+		'units': {
+			'Kg -> Lbs': kg_to_lbs,
+			'Lbs -> Kg': lbs_to_kg
+		}
+	},
+	{
+		'name': "Lampotila",
+		'units': {
+			'C -> F': c_to_f,
+			'F -> C': f_to_c
+		}
+	},
 ]
 
 def menu():
 	for i, u in enumerate(units):
-		print(str(i + 1) + ") " + u)
+		print(str(i + 1) + ") " + u['name'])
 	print("0) Poistu")
 	return int(input("Valitse muunto: "))
 
@@ -28,7 +40,7 @@ def main():
 	choice = 1
 	while choice > 0:
 		choice = menu()
-		print(units[choice - 1] + " valittu")
+		print(units[choice - 1]['name'] + " valittu")
 
 if __name__ == "__main__":
 	main()
