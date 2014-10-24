0218c20bee034faed988c98d5ba151a100f8a5f5
Aleksi Kervinen <aleksi.kervinen@lut.fi>
2014-10-24 13:54:54 +0300

    added basic conversion functions

--- muunnin.py
+++ muunnin.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
+def kg_to_lbs(kg):
+	return kg * 2.205
+	
+def lbs_to_kg(lbs):
+	return lbs/2.205
+
+def c_to_f(c):
+	return c * 9.0/5.0 + 32
+
+def f_to_c(f):
+	return (f - 32) * 5.0/9.0
+
 def main():
-	pass
+	print("Tervetuloa Muuntimeen!")
 
 if __name__ == "__main__":
 	main()
