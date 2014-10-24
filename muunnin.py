3a9bd8dafd601a5a6ff4e5ebdb9bc740cb2c78f4
Aleksi Kervinen <aleksi.kervinen@lut.fi>
2014-10-24 13:59:57 +0300

    basic menu structure

--- muunnin.py
+++ muunnin.py
@@ -11,9 +11,24 @@ def c_to_f(c):
 
 def f_to_c(f):
 	return (f - 32) * 5.0/9.0
+	
+units = [
+	"Paino",
+	"Lampotila"
+]
+
+def menu():
+	for i, u in enumerate(units):
+		print(str(i + 1) + ") " + u)
+	print("0) Poistu")
+	return int(input("Valitse muunto: "))
 
 def main():
 	print("Tervetuloa Muuntimeen!")
+	choice = 1
+	while choice > 0:
+		choice = menu()
+		print(units[choice - 1] + " valittu")
 
 if __name__ == "__main__":
 	main()
